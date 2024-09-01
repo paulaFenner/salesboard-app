b4395fa4ef7f17b8f0d8c115eaeb2feb36a3615e
abaltaza <andrebaltazarpinto@gmail.com>
2024-09-01 13:49:08 +0100

    Set up of the ligh and dark-mode

--- index.js
+++ index.js
@@ -11,3 +11,9 @@ let productB = {
     revenue: 300,
     commision: 75
 }
+
+const toggle = document.getElementById('darkmode-toggle');
+
+toggle.addEventListener('change', function() {
+    document.body.classList.toggle('dark-mode', toggle.checked);
+});
