efb21c9c607dbf813e628fc26a948832490636a7
abaltaza <andrebaltazarpinto@gmail.com>
2024-09-06 17:40:31 +0100

    live-sales board ✅

--- index.js
+++ index.js
@@ -17,3 +17,42 @@ const toggle = document.getElementById('darkmode-toggle');
 toggle.addEventListener('change', function() {
     document.body.classList.toggle('dark-mode', toggle.checked);
 });
+
+/* Variables of fire & star */
+const star = document.getElementById("star")
+const fire = document.getElementById("fire")
+console.log(star)
+
+/* live sales */
+let liveSalesBoard = document.getElementById("live-sales")
+
+/*live achievements board */
+let liveAchievementsBoard = document.getElementById("live-achievements")
+console.log(liveAchievementsBoard)
+/* Total revenue board */
+let totalRevenueBoard = document.querySelector(".total-revenue")
+console.log(totalRevenueBoard)
+/* Reset Data */
+const resetData = document.querySelector(".reset-button")
+console.log(resetData)
+
+
+
+// functions 
+function addStar() {
+    liveSalesBoard.innerText += productA.emoji;
+}
+
+function addFire() {
+    liveSalesBoard.innerText += productB.emoji;
+}
+
+function resetDataAll() {
+    liveSalesBoard.innerText = ""
+}
+
+// add fire and star
+star.addEventListener("click", addStar)
+fire.addEventListener("click", addFire)
+//reset data
+resetData.addEventListener("click", resetDataAll)
